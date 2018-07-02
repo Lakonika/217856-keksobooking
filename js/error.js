4667d1773b252eebd94e23c8bd75321bda32b5b3
Lakonika <oplokhotnyuk@gmail.com>
2018-07-02 18:03:50 +0900

    Task 22

--- js/error.js
+++ js/error.js
@@ -1,14 +1,21 @@
 'use strict';
 
 (function () {
-  var errorMessage = document.createElement('div');
-  var closeButton = errorMessage.createElement('button');
-  errorMessage.classList.add('error');
-  closeButton.classList.add('error__close');
-  errorMessage.textContent = 'text';
-  errorMessage.appendChild(closeButton);
-  closeButton.addEventListener('click', function () {
-    errorMessage.remove();
-  });
-  return errorMessage;
+  var createPopup = function () {
+    var errorMessage = document.createElement('div');
+    var closeButton = errorMessage.createElement('button');
+    errorMessage.classList.add('error');
+    closeButton.classList.add('error__close');
+    errorMessage.textContent = 'text';
+    errorMessage.appendChild(closeButton);
+
+    closeButton.addEventListener('click', function () {
+      errorMessage.remove();
+    });
+    return errorMessage;
+  };
+
+  window.error = {
+    createPopup: createPopup
+  };
 })();
